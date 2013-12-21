a903fb42cddf416cf2d65a4d7810b510a8b8ec21
WildGorn <WildGorn@gmail.com>
2013-12-21 19:45:05 +0200

    test

--- src/ua/pp/sar/lab1/lab1.java
+++ src/ua/pp/sar/lab1/lab1.java
@@ -1,7 +1,24 @@
 package ua.pp.sar.lab1;
 
+import javax.print.DocFlavor;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by WildGorn on 21.12.13.
  */
 public class lab1{
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("ВВедите имя");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        StringBuffer sb = new StringBuffer();
+        sb.append("Hello,");
+        sb.append(" ");
+        sb.append(s);
+        System.out.println(sb.toString());
+    }
 }
