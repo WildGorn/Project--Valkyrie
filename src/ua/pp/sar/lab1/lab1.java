a7c59f0370cfe0fc1f6d6869507b9d96df225e6e
WildGorn <WildGorn@gmail.com>
2013-12-21 21:53:38 +0200

    test

--- src/ua/pp/sar/lab1/lab1.java
+++ src/ua/pp/sar/lab1/lab1.java
@@ -7,17 +7,21 @@ import java.io.InputStreamReader;
 /**
  * Created by WildGorn on 21.12.13.
  */
-public class lab1{
+public class lab1 {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("ВВедите имя");
+        System.out.println("Введите имя");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
         StringBuffer sb = new StringBuffer();
-        sb.append("Hello,");
-        sb.append(" ");
+        sb.append("Hello, ");
         sb.append(s);
-        System.out.println(sb.toString());
+        sb.append("!");
+        if (s != null) {
+            System.out.println(sb.toString());
+        } else {
+            System.out.println("Привет незнакомец");
+        }
     }
 }
